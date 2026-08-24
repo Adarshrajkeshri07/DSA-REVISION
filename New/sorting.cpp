725ff8674da920420c407475fbf87e592e7f6407
Adarshrajkeshri07 <rajadarshkeshri@gmail.com>
2026-08-24 20:59:36 +0530

    selection

--- New/sorting.cpp
+++ New/sorting.cpp
@@ -4,13 +4,14 @@ using namespace std ;
 int main(){
     int  a[6] = {4,1,5,4,9,3} ; 
     int n = 6 ; 
-    for(int i = 0 ; i < n -1 ; i++){
-        
-        for(int j = 0 ;  j < n - i - 1 ; j++){
-            if(a[j] > a[j+1]){
-                swap(a[j] ,a[j+1]);
-            }
+    for(int i = 0 ; i < n  ; i++){
+        int smallest = i ; 
+        for(int j = i + 1 ;  j < n  ; j++){
+        if(a[j] < a[smallest]){
+            smallest = j ; 
         }
+        }
+        swap(a[i], a[smallest]);
     }
     for(int i = 0 ; i < n ; i++){
         cout << a[i] ; 
