84f995fbd5a01d9bffef1bddbf191af01fdf88ef
Adarshrajkeshri07 <rajadarshkeshri@gmail.com>
2026-08-24 21:11:48 +0530

    insertion

--- New/sorting.cpp
+++ New/sorting.cpp
@@ -4,15 +4,17 @@ using namespace std ;
 int main(){
     int  a[6] = {4,1,5,4,9,3} ; 
     int n = 6 ; 
-    for(int i = 0 ; i < n  ; i++){
-        int smallest = i ; 
-        for(int j = i + 1 ;  j < n  ; j++){
-        if(a[j] < a[smallest]){
-            smallest = j ; 
-        }
-        }
-        swap(a[i], a[smallest]);
-    }
+for(int  i = 1 ; i < n ; i++){
+    int curr  = a[i] ; 
+    int pre = i - 1 ; 
+   while(pre >= 0 && a[pre] > curr){
+
+       a[pre + 1] = a[pre];
+       pre--;
+   }
+    
+    a[pre+1] = curr ;
+}
     for(int i = 0 ; i < n ; i++){
         cout << a[i] ; 
     }
