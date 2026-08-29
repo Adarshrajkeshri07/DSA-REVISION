ab2aeb16cf08a1cf695c7ea3a92c5fe6819333ce
Adarshrajkeshri07 <rajadarshkeshri@gmail.com>
2026-08-29 23:28:12 +0530

    rearrange

--- array question/colorsort.cpp
+++ array question/colorsort.cpp
@@ -1,27 +1,36 @@
 #include<iostream>
-#include<algorithm>
 using namespace std ; 
+int mat(int a[] ,int size){
+    int one = 0 ,two = 0 , zero = 0 ; 
+    for(int i = 0 ; i < size ; i++){
+        if(a[i]==0){
+            zero++;
+        }else  
+        if(a[i]==1){
+            one++ ;
+        }else {
+            two++;
+        }
+        int index = 0 ; 
+        for(int i = 0 ; i < zero ; i++){
+            a[index++] = 0 ;  
+        }
+        for (int i = 0; i < one; i++)
+        {
+            a[index++] = 1;
+        }
+        for (int i = 0; i < two; i++)
+        {
+            a[index++] = 2;
+        }
+    }
+    for(int i = 0 ; i < size ; i++){
+        cout <<a[i] ; 
+    }
+    return 0 ; 
+}
 int main(){
-    int a[6] = {0,2,1,0,2,1} ; 
-    int n = 6 ; 
-    int st = 0 , mid = 0 , end = n - 1 ; 
-   while (mid <= end)
-   {
-    if(a[mid] == 0 ){
-        swap(a[mid] , a[st]) ; 
-        st++ , mid++ ; 
-    }
-    else if(a[mid] == 1){
-        mid++ ; 
-    }
-    else {
-        swap(a[mid] , a[end]) ; 
-        end-- ; 
-    }
-   }
-   for(int i = 0 ; i < n ; i++){
-    cout<< a[i] << " " ; 
-   }
-   return 0 ; 
-   
+    int a[5] = {2,0,1,0,2} ; 
+    mat(a ,5) ;
+    return 0 ; 
 }
