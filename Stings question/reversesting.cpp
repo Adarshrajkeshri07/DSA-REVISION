fadf63db69bac26679e83a956757d4c4b020c0bf
Adarshrajkeshri07 <rajadarshkeshri@gmail.com>
2026-09-05 14:29:36 +0530

    recoorection

--- Stings question/reversesting.cpp
+++ Stings question/reversesting.cpp
@@ -4,7 +4,8 @@
 using namespace std ; 
 int main(){
  string s ; 
- cin >> s ; 
+ cout << " enter the strings : -" ; 
+getline(cin ,s);
 int st = 0 , end = s.length() - 1 ; 
 while(st < end){
     swap(s[st] , s[end]) ; 
