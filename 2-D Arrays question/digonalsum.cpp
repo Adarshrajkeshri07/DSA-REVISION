ed3923d4ccee98644fdae24654ce32b46dc4cebf
Adarshrajkeshri07 <rajadarshkeshri@gmail.com>
2026-09-10 15:45:15 +0530

    digonal sum of 2d matrix

--- 2-D Arrays question/digonalsum.cpp
+++ 2-D Arrays question/digonalsum.cpp
@@ -1,15 +1,16 @@
 #include<iostream>
-using namespace std ;
+using namespace std ; 
 int main(){
-    int matrix[4][4] = {{1,2,3,4},{4,5,6,7},{7,8,9,10},{10,11,12,11}};
+    int matrix[4][4] = {{1, 2, 3, 4}, {4, 5, 6, 7}, {7, 8, 9, 10}, {10, 11, 12, 11}};
     int n = 4 ; 
-    int digonalsum = 0 ; 
+    int sum = 0 ; 
     for(int i = 0 ; i < n ; i++){
-        digonalsum += matrix[i][i] ; 
-        if(i != n - 1 - i){
-            digonalsum += matrix[i][n-1-i] ; 
+        sum += matrix[i][i] ; 
+        if( i != n - 1 - i){
+            sum += matrix[i][n - i - 1] ; 
         }
     }
-    cout << " the digonal sum of this matrix is " << digonalsum  ; 
-    return 0  ;
+    cout << " the sum of digonal element of the matrix is  : " << sum ; 
+    return 0 ; 
+
 }
